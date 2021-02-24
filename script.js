1ee8d79c25767b58283f7e4d27d9f5b551595de4
moon pie <elidakirigo@gmail.com>
2021-02-24 12:17:23 +0300

    ingred...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const file_input = ['a_example', 'b_little_bit_of_everything', 'c_many_ingredients', 'd_many_pizzas.in', 'e_many_teams.in']
+const file_input = ['a_example', 'b_little_bit_of_everything', 'c_many_ingredients', 'd_many_pizzas', 'e_many_teams']
 const data = fs.readFileSync(file_input[0], 'utf8')
 
 const data_set = data.split("\n").splice(1, )
@@ -11,8 +11,16 @@ data_set.forEach((element, i) => {
 
     // console.log(new_data);
 });
-console.log(data_set, new_data);
-
+// console.log(data_set, new_data);
+const ingredients = {
+    all_ingredients:[]
+}
 new_data.forEach(element => {
     
+    element.splice(1,).forEach(el => {
+        ingredients.all_ingredients.push(el)
+    });
 });
+ingredients.uniq_ingredients =[...new Set(ingredients.all_ingredients)]
+
+
